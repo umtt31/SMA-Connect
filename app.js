c8884caab4c691434a622d731147284da4c3aeea
umtt31 <umutcan.adiguzel@outlook.com>
2023-12-05 21:13:58 +0300

    database connection ve user schema

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-// get the client
-const mysql = require('mysql2');
+const express = require('express');
+const methodOverride = require('method-override');
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'test'
-});
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method', { methods: ['GET', 'POST'] }));
+
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+
+app.listen(port, () => {
+    console.log(`Server running on port : ${port}`);
+  });
---
+++ db.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "test",
+});
+
+module.exports = connection;
+
+
