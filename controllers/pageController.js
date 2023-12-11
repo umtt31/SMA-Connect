d778d5ff88c494f8406e52c592916da828acb843
umtt31 <umutcan.adiguzel@outlook.com>
2023-12-11 15:20:12 +0300

    page routes + html to ejs

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,1 +1,32 @@
+exports.getHomePage = (req, res) => {
+    res.render('index.ejs')
+}
+
+exports.getMainPage = (req, res) => {
+    res.render('main.ejs')
+}
+
+exports.getAboutPage = (req, res) => {
+    res.render('about.ejs')
+}
+
+exports.getBlogPage = (req, res) => {
+    res.render('blog.ejs')
+}
+
+exports.getContactPage = (req, res) => {
+    res.render('contact.ejs')
+}
+
+exports.getDonatePage = (req, res) => {
+    res.render('donate.ejs')
+}
+
+exports.getFeedPage = (req, res) => {
+    res.render('feed-page.ejs')
+}
+
+exports.getGalleryPage = (req, res) => {
+    res.render('gallery.ejs')
+}
 
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -4,10 +4,13 @@ const pageController = require("../controllers/pageController.js");
 
 const router = express.Router();
 
-// router.route("").get(pageController.getHomePage);
-// router.route("/register").post(userController.register);
-// router.route("/login").post(userController.login);
-// router.route("/:id").delete(userController.deleteUser);
-// router.route("/:id").put(userController.updateUser);
+router.route("").get(pageController.getHomePage);
+router.route("/main").get(pageController.getMainPage);
+router.route("/about").get(pageController.getAboutPage);
+router.route("/blog").get(pageController.getBlogPage);
+router.route("/contact").get(pageController.getContactPage);
+router.route("/donate").get(pageController.getDonatePage);
+router.route("/feed").get(pageController.getFeedPage);
+router.route("/gallery").get(pageController.getGalleryPage);
 
 module.exports = router;
